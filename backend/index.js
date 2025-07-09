fd12c963b096d78bc1023e352724c2a54d7b4f16
kxcvbnm <khunanont.ti@gmail.com>
2025-07-09 22:28:18 +0700

    its worked

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from "path"
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
 import { connectDB } from './db/connectDB.js'
-import path from "path"
 
 dotenv.config();
 
@@ -12,14 +12,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
-    app.use(
-        cors({
-            origin: "http://localhost:5173",
-            credentials: true
-        })
-    );
-}
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    })
+);
+
 
 app.use(express.json()); // allow to parse json
 app.use(cookieParser()); // allow us to parse incoming cookies
@@ -27,9 +27,9 @@ app.use(cookieParser()); // allow us to parse incoming cookies
 app.use("/api/auth", authRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/*any", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
